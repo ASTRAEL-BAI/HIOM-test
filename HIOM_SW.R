8defcfaf45d2148218e2c0f09c071962e1cd1a7e
Astrael <AstraelBAI@gmail.com>
2025-06-26 18:30:02 +0800

    Updated

--- HIOM_SW.R
+++ HIOM_SW.R
@@ -112,7 +112,7 @@ plot.histo= function(x,min,max,xlab='')
 
 ##############
 
-scenario=3  # set scenario
+scenario=4  # set scenario
 
 # scenario 1: removed from manuscript
 # scenario 2 : figure 4
---
+++ TestCode.R
@@ -0,0 +1,2 @@
+mean(attention[1:400])
+mean(attention[401:800])
